3ae058aebde7c8f711f24897e7ea3f5140f14e75
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-14 22:27:24 +0000

    routing作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,83 +1,99 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'posts/edit'
-    get 'posts/update'
-  end
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/show'
-    get 'comments/edit'
-    get 'comments/update'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :user do
-    get 'tags/new'
-    get 'tags/create'
-    get 'tags/destroy'
-  end
-  namespace :user do
-    get 'relationships/index'
-    get 'relationships/create'
-    get 'relationships/destroy'
-  end
-  namespace :user do
-    get 'favourites/index'
-    get 'favourites/destroy'
-  end
-  namespace :user do
-    get 'ranking/index'
-  end
-  namespace :user do
-    get 'post/new'
-    get 'post/index'
-    get 'post/create'
-    get 'post/show'
-    get 'post/update'
-    get 'post/destroy'
-  end
-  namespace :user do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/withdraw'
-    get 'users/unsubscribe'
-  end
-  namespace :user do
-    get 'comments/index'
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :user do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :user, controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+
+  devise_for :user, skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
 }
 
-  devise_for :admin, controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-  namespace :user do
-    get 'rankings/index'
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :posts,only: [:edit,:update]
+    resources :comments,only: [:show,:edit,:update]
+    resources :users,only: [:index,:show,:edit,:update]
   end
-  namespace :user do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/create'
-    get 'posts/show'
-    get 'posts/update'
-    get 'posts/destroy'
+
+  scope module: :user do
+    root to:"homes#top"
+    get'about' => 'homes#about', as:'about'
+    resources :tags,only: [:new,:create,:destroy]
+    resources :relationships,only: [:index,:create,:destroy]
+    resources :favourites,only: [:index,:destroy]
+    resources :ranking,only: [:index]
+    resources :posts,only: [:new,:index,:create,:show,:update,:destroy]
+    get 'users/my_page' => 'users#show'
+    get 'users/my_page/edit' => 'users#edit'
+    patch 'users/my_page/edit' => 'users#update'
+    get 'users/withdraw' => 'users#withdraw'
+    patch 'users/unsubscribe' => 'users#unsubscribe'
   end
+
   # devise_for :admins
   # devise_for :users
       #   t.string :name
       # t.text :introduction
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+  # namespace :admin do
+    # get 'posts/edit'
+    # get 'posts/update'
+  # end
+  # namespace :admin do
+  #   get 'comments/show'
+  #   get 'comments/edit'
+  #   get 'comments/update'
+  # end
+  # namespace :admin do
+  #   get 'users/index'
+  #   get 'users/show'
+  #   get 'users/edit'
+  #   get 'users/update'
+  # end
+  # namespace :user do
+  #   get 'tags/new'
+  #   get 'tags/create'
+  #   get 'tags/destroy'
+  # end
+  # namespace :user do
+  #   get 'relationships/index'
+  #   get 'relationships/create'
+  #   get 'relationships/destroy'
+  # end
+  # namespace :user do
+  #   get 'favourites/index'
+  #   get 'favourites/destroy'
+  # end
+  # namespace :user do
+  #   get 'ranking/index'
+  # end
+  # namespace :user do
+  #   get 'users/show'
+  #   get 'users/edit'
+  #   get 'users/update'
+  #   get 'users/withdraw'
+  #   get 'users/unsubscribe'
+  # end
+  # namespace :user do
+  #   get 'comments/index'
+  #   get 'comments/create'
+  #   get 'comments/destroy'
+  # end
+  # namespace :user do
+  #   get 'homes/top'
+  #   get 'homes/about'
+  # end
+  # namespace :user do
+  #   get 'rankings/index'
+  # end
+  # namespace :user do
+  #   get 'posts/new'
+  #   get 'posts/index'
+  #   get 'posts/create'
+  #   get 'posts/show'
+  #   get 'posts/update'
+  #   get 'posts/destroy'
+  # end
