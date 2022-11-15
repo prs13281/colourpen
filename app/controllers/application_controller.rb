ce32c1ac0e3ea987fdc555f1a01a7dc008cc602c
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-15 11:27:57 +0000

    admin ログイン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+   case resource
+    when Admin
+     admin_users_path
+    when User
+     root_path
+   end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource == :admin
+     new_admin_session_path
+    elsif resource ==  :user
+     root_path
+    end
+  end
+
 end
--- db/migrate/20221114112443_devise_create_users.rb
+++ db/migrate/20221114112443_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false, default: ""
+      t.text :introduction
+      t.boolean :is_deleted,        null: false, default: "false"
 
       ## Recoverable
       t.string   :reset_password_token
