71e513d17b6fed1779f5b161dc3e135154460676
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-19 13:49:51 +0000

    admin/user/edit 途中

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,11 +8,19 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+     redirect_to admin_user_path(@user.id), notice: 'You have updated book successfully'
+    else
+     @user = User.find(params[:id])
+     render :edit
+    end
   end
-  
+
   #ストロングパラメーター
   def user_params
     params.require(:user).permit(:name,  :email, :is_deleted)
