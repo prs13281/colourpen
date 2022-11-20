703ebcd70b6945bab2985dfac2ab026102fc0a2b
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-20 07:31:01 +0000

    view/user/show 途中

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,11 +1,19 @@
 class User::UsersController < ApplicationController
   def show
+    @user = current_user
+    @posts = @user.posts
   end
 
   def edit
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+     redirect_to user_my_page_path(current_user), notice: 'You have updated book successfully'
+    else
+     render :edit
+    end
   end
 
   def withdraw
@@ -16,6 +24,6 @@ class User::UsersController < ApplicationController
 
   #ストロングパラメーター
   def user_params
-    params.require(:user).permit(:name, :introduction, :email, :is_deleted)
+    params.require(:user).permit(:name, :introduction, :email)
   end
 end
