a8f51890be7b845d1d374b5b8da0a404e4947711
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-27 10:46:35 +0000

    user/posts/newのgooglemapAPI途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import Turbolinks from "turbolinks" //新規画像投稿ではコメントアウトする
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
@@ -12,6 +12,9 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+//import './preview'  // 新規投稿に画像表示の追記
+
+
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,19 @@
+//新規投稿画像表示
+//if (document.URL.match(/new/)){
+//  document.addEventListener('DOMContentLoaded', () => {
+//    const createImageHTML = (blob) => {
+//      const imageElement = document.getElementById('new-image');
+//      const blobImage = document.createElement('img');
+//      blobImage.setAttribute('class', 'new-img')
+//      blobImage.setAttribute('src', blob);
+//      imageElement.appendChild(blobImage);
+//    };
+
+//    document.getElementById('post_image').addEventListener('change', (e) =>{
+//      const file = e.target.files[0];
+//      const blob = window.URL.createObjectURL(file);
+//      createImageHTML(blob);
+//    });
+//  });
+//} //新規投稿画像表示の追記 エラーなのでまた後でする
+
