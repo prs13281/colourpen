c7df16dd73cd4a3f0476626d0e1d284d768795b2
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-11-29 22:03:49 +0000

    user/post/showコメント削除機能途中

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,10 +1,16 @@
 class User::CommentsController < ApplicationController
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+
   def create
     # コメントをする対象の投稿(travel_record)のインスタンスを作成
     @post =Post.find(params[:post_id])
     #投稿に紐づいたコメントを作成
-    @comment = @post.comments.new(comment_params)
+    @comment = @post.comments.build(comment_params)
     # コメント投稿者(user)のidを代入
     @comment.user_id = current_user.id
     @comments = @post.comments
@@ -18,9 +24,11 @@ class User::CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     #@comment.post_idから親のpost情報を取得し@comments = @post.comments
+    #@post = Post.find(params[:post_id])
+    #@comments = 
     @comment.destroy
     flash.now[:notice] = "コメントを削除しました。"
-    render :index
+    render :show
   end
 
   private
