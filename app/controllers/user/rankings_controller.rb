382e639064b7492e63e69441ca6c0c9513560722
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-04 12:52:36 +0000

    user/ranking機能途中

--- app/controllers/user/rankings_controller.rb
+++ app/controllers/user/rankings_controller.rb
@@ -1,4 +1,9 @@
 class User::RankingsController < ApplicationController
+  
   def index
+    # ユーザーの投稿数ランキング
+   @user_post_rankings = User.where(id: Post.group(:user_id).order('count(user_id) desc').pluck(:user_id))
+    # 投稿のコメント数ランキング
+   @post_comment_ranking = Post.find(Comment.group(:post_id).order('count(post_id) desc').pluck(:post_id))
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,5 @@
 class Favorite < ApplicationRecord
 
   belongs_to :user
-  #belongs_to :ranking
   belongs_to :post
 end
