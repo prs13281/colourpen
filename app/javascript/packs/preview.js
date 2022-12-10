9769d7b3edbd4d9156e9cfd90a1574b53edf4790
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-10 12:29:59 +0000

    user/post/new写真プレビュー完了

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,8 +1,18 @@
 //新規投稿画像表示
 if (document.URL.match(/new/)){
   document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('postimage').addEventListener('change', (e) =>{
-      console.log(e);
+    const createImageHTML = (blob) => {
+      const imageElement = document.getElementById('new-image');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'new-img')
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+    };
+
+    document.getElementById('post_image').addEventListener('change', (e) =>{
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
     });
   });
 
