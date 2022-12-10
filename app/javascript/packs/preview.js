e439309865281d02c190f42e0eab6f2297b00be2
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-10 12:56:50 +0000

    user/post/edit写真プレビュー完了

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -21,6 +21,30 @@ if (document.URL.match(/new/)){
   });
 
 }
+
+//投稿編集画面画像表示
+if (document.URL.match(/edit/)){
+  document.addEventListener('DOMContentLoaded', () => {
+    const createImageHTML = (blob) => {
+      const imageElement = document.getElementById('new-image');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('class', 'new-img')
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+    };
+
+    document.getElementById('post_image').addEventListener('change', (e) =>{
+      const imageContent = document.querySelector('img');
+      if (imageContent){
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+  });
+
+}
 //    const createImageHTML = (blob) => {
 //      const imageElement = document.getElementById('new-image');
 //      const blobImage = document.createElement('img');
