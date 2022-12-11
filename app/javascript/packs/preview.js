f0c67a7c1ede2c440e5d2eaedd2438a003a01366
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-11 05:40:42 +0000

    user/edit写真プレビュー完了

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,4 +1,4 @@
-//新規投稿画像表示
+//新規投稿、編集画像表示
 if (document.URL.match(/new|edit/)){
   document.addEventListener('DOMContentLoaded', () => {
     const createImageHTML = (blob) => {
@@ -9,40 +9,52 @@ if (document.URL.match(/new|edit/)){
       imageElement.appendChild(blobImage);
     };
 
-    document.getElementById('post_image').addEventListener('change', (e) =>{
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
+    var postImage = document.getElementById('post_image')
+    var profileImage = document.getElementById('profile_image')
+    var element
+    if (postImage) {
+      element = postImage
+    }
+    if (profileImage) {
+      element = profileImage
+    }
+    if (element) {
+      element.addEventListener('change', (e) =>{
+        const imageContent = document.querySelector('img');
+        if (imageContent){
+          imageContent.remove();
+        }
+        const file = e.target.files[0];
+        const blob = window.URL.createObjectURL(file);
+        createImageHTML(blob);
+      });
+    }
   });
 
 }
 
-//ユーザー編集画像表示
-if (document.URL.match(/edit/)){
-  document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
-      const imageElement = document.getElementById('user_profile_image');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'user_profile_image')
-      blobImage.setAttribute('src', blob);
-      imageElement.appendChild(blobImage);
-    };
+// //ユーザー編集画像表示
+// if (document.URL.match(/edit/)){
+//   document.addEventListener('DOMContentLoaded', () => {
+//     const createImageHTML = (blob) => {
+//       const imageElement = document.getElementById('user_profile_image');
+//       const blobImage = document.createElement('img');
+//       blobImage.setAttribute('class', 'user_profile_image')
+//       blobImage.setAttribute('src', blob);
+//       imageElement.appendChild(blobImage);
+//     };
 
-    document.getElementById('user_image').addEventListener('change', (e) =>{
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
-  });
+//     document.getElementById('user_image').addEventListener('change', (e) =>{
+//       const imageContent = document.querySelector('img');
+//       if (imageContent){
+//         imageContent.remove();
+//       }
+//       const file = e.target.files[0];
+//       const blob = window.URL.createObjectURL(file);
+//       createImageHTML(blob);
+//     });
+
+
 
 // }
 //    const createImageHTML = (blob) => {
