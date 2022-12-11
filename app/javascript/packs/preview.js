ea35babcb0ad892ee744eb0702ecc353d75eeb8a
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-11 09:02:51 +0000

    フォントの変更

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,4 +1,4 @@
-//新規投稿、編集画像表示
+//新規投稿と編集、マイページの写真プレビュー表示
 if (document.URL.match(/new|edit/)){
   document.addEventListener('DOMContentLoaded', () => {
     const createImageHTML = (blob) => {
@@ -9,8 +9,10 @@ if (document.URL.match(/new|edit/)){
       imageElement.appendChild(blobImage);
     };
 
+    // 投稿とマイページで表示させたいのでふたつ必要
     var postImage = document.getElementById('post_image')
     var profileImage = document.getElementById('profile_image')
+
     var element
     if (postImage) {
       element = postImage
@@ -32,44 +34,3 @@ if (document.URL.match(/new|edit/)){
   });
 
 }
-
-// //ユーザー編集画像表示
-// if (document.URL.match(/edit/)){
-//   document.addEventListener('DOMContentLoaded', () => {
-//     const createImageHTML = (blob) => {
-//       const imageElement = document.getElementById('user_profile_image');
-//       const blobImage = document.createElement('img');
-//       blobImage.setAttribute('class', 'user_profile_image')
-//       blobImage.setAttribute('src', blob);
-//       imageElement.appendChild(blobImage);
-//     };
-
-//     document.getElementById('user_image').addEventListener('change', (e) =>{
-//       const imageContent = document.querySelector('img');
-//       if (imageContent){
-//         imageContent.remove();
-//       }
-//       const file = e.target.files[0];
-//       const blob = window.URL.createObjectURL(file);
-//       createImageHTML(blob);
-//     });
-
-
-
-// }
-//    const createImageHTML = (blob) => {
-//      const imageElement = document.getElementById('new-image');
-//      const blobImage = document.createElement('img');
-//      blobImage.setAttribute('class', 'new-img')
-//      blobImage.setAttribute('src', blob);
-//      imageElement.appendChild(blobImage);
-//    };
-
-//    document.getElementById('post_image').addEventListener('change', (e) =>{
-//      const file = e.target.files[0];
-//      const blob = window.URL.createObjectURL(file);
-//      createImageHTML(blob);
-//    });
-//  });
-//} //新規投稿画像表示の追記 エラーなのでまた後でする
-
