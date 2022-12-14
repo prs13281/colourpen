3ea0156e982ba429e1199c6841cae8c3fa53efe8
prs13281 <ec2-user@ip-172-31-17-148.ap-northeast-1.compute.internal>
2022-12-14 22:34:46 +0000

    user/post/show,admin/post/showレイアウト調整

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,8 +7,11 @@ class Admin::PostsController < ApplicationController
     # @user = User.find(params[:id])
     #投稿した人の名前表示
     @user = @post.user
-    # コメント一覧表示で使用する全コメントデータを代入（新着順で表示）
-    @comments = @post.comments.order(created_at: :asc)
+    # コメント一覧表示で使用する全コメントデータを代入（新着順で表示
+    # @comments = @post.comments.order(created_at: :asc)
+     # コメント一覧表示で使用する全コメントデータを代入（新着順で表示）
+    @comments = @post.comments.order(created_at: :asc).first(3)
+    @comments_next = @post.comments.order(created_at: :asc).offset(3)
     # 緯度経度を保存する
     gon.lng = @post.longitude
     gon.lat = @post.latitude
